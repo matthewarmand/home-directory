252ec5c5d5cd2ce29e5b45e422fa49e060dbd0da
Matt Armand <marmand68@gmail.com>
2020-01-21 16:49:45 -0500

    Add some handy utility scripts to work box

--- .zshrc-machines/marmand-metametrics.sh
+++ .zshrc-machines/marmand-metametrics.sh
@@ -1,3 +1,35 @@
-#!/usr/bin/zsh
+#!/usr/bin/bash -e
 
 alias tizonia='source $HOME/development/personal/docker-tizonia/docker-tizonia'
+
+open-jira-tabs() {
+  if [[ ! -f "$1" ]]; then
+    echo "Unable to find list of tickets in file ($1). Please specify."
+  else
+    while read -r l; do
+      if [[ ${l:0:1} != "#" ]]; then
+        firefox-developer-edition -new-tab "https://metametrics.atlassian.net/browse/$l"
+      fi
+    done <"$1"
+  fi
+}
+
+mm-git-config() {
+  orig_dir=$(pwd)
+  [[ -d "$1" ]] && base_dir="$1" || base_dir="$HOME/development/mm"
+  echo "Using $base_dir as base directory"
+
+  find "$base_dir" -type d -name ".git" | while IFS=$'\n' read -r d; do
+    cd "$(dirname "$d")"
+    git_config=$(git config -l)
+    if echo "$git_config" | grep -q "metametrics" ; then
+      if ! echo "$git_config" | grep -q "@lexile.com" ; then
+        git config --local --replace user.name "Matthew Armand"
+        git config --local --replace user.email marmand@lexile.com
+        echo "Updated config for $(pwd)"
+      fi
+    fi
+    cd "$base_dir"
+  done
+  cd "$orig_dir"
+}
