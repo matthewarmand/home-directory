1805fdb3bbfbfb4a7967af9a4f794bf305553657
Matt Armand <marmand68@gmail.com>
2023-06-02 13:02:50 -0400

    added lock warning, extracted swayidle logic for easy kill-and-restart, refactored wallpaper to expect the same path on every machine (update when pulling this commit)

---
+++ .config/sway/scripts/idle.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -e
+# This will lock your screen after 5 minutes of inactivity, and put the
+# system to sleep after 2 hours of inactivity. It will also lock your
+# screen before your computer goes to sleep. It will also give a warning
+# 5 seconds before locking.
+# required-arch-package :: swayidle
+# required-arch-package :: yad
+
+if ! prep swayidle; then
+  lock_command='source ~/.config/sway/scripts/pause-and-lock.sh'
+  swayidle -w \
+    timeout 295 'yad --timeout 5 --timeout-indicator top --text "Machine will lock in 5 seconds" --title "Lock Warning"' \
+    timeout 300 "$lock_command" \
+    timeout 7200 'systemctl suspend' \
+    before-sleep "$lock_command"
+fi
--- .config/sway/scripts/pause-and-lock.sh
+++ .config/sway/scripts/pause-and-lock.sh
@@ -2,7 +2,7 @@
 # required-arch-package :: playerctl
 # required-arch-package :: swaylock
 
-lock_cmd="swaylock -Fei $1"
+lock_cmd="swaylock -Fei /usr/share/wallpapers/wallpaper.jpg"
 if [ "$(playerctl -p spotify status 2>/dev/null)" = "Playing" ]; then
   playerctl -p spotify pause
   eval "$lock_cmd"
