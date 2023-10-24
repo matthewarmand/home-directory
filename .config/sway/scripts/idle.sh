0d8b4a1276ab2b6ab11fb4da3ee22c524dece41c
Matt Armand <marmand68@gmail.com>
2023-10-23 22:12:46 -0400

    remove yad in favor of swaynag in idle config

--- .config/sway/scripts/idle.sh
+++ .config/sway/scripts/idle.sh
@@ -4,13 +4,12 @@
 # screen before your computer goes to sleep. It will also give a warning
 # 5 seconds before locking.
 # required-arch-package :: swayidle
-# required-arch-package :: yad
 
 if ! pgrep swayidle; then
   lock_command="source ~/.config/sway/scripts/pause-and-lock.sh"
   swayidle -w \
-    timeout 295 "yad --timeout 5 --timeout-indicator top --text 'Machine will idle-lock in 5 seconds' --title 'Lock Warning'" \
-    timeout 300 "$lock_command" \
+    timeout 295 "swaynag -t warning -m 'Machine will idle-lock in 5 seconds' &" \
+    timeout 300 "pkill swaynag; $lock_command" \
     timeout 7200 "systemctl suspend" \
     before-sleep "$lock_command" &
 else
