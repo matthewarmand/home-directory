5e9ceb6ae56c357eae0d032418dc6182ebf52875
Matt Armand <marmand68@gmail.com>
2023-10-25 13:32:14 -0400

    don't attempt to lock if we're already locked

--- .config/sway/scripts/pause-and-lock.sh
+++ .config/sway/scripts/pause-and-lock.sh
@@ -2,11 +2,13 @@
 # required-arch-package :: playerctl
 # required-arch-package :: swaylock
 
-lock_cmd="swaylock -Fei /usr/share/wallpapers/wallpaper.jpg"
-if [ "$(playerctl -p spotify status 2>/dev/null)" = "Playing" ]; then
-  playerctl -p spotify pause
-  eval "$lock_cmd"
-  playerctl -p spotify play
-else
-  eval "$lock_cmd -f"
+if ! pgrep swaylock; then
+  lock_cmd="swaylock -Fei /usr/share/wallpapers/wallpaper.jpg"
+  if [ "$(playerctl -p spotify status 2>/dev/null)" = "Playing" ]; then
+    playerctl -p spotify pause
+    eval "$lock_cmd"
+    playerctl -p spotify play
+  else
+    eval "$lock_cmd -f"
+  fi
 fi
