1e4eb4f3111028d513b81818294fa32585cb040f
Matt Armand <marmand68@gmail.com>
2025-01-06 13:49:53 -0500

    allow for all/multiple players in pause/lock. Expand statusbar block too

--- .config/sway/scripts/pause-and-lock.sh
+++ .config/sway/scripts/pause-and-lock.sh
@@ -5,10 +5,19 @@
 pkill swaynag || true
 if ! pgrep swaylock; then
   lock_cmd="swaylock -Fei /usr/share/wallpapers/wallpaper.jpg"
-  if [ "$(playerctl -p spotify status 2>/dev/null)" = "Playing" ]; then
-    playerctl -p spotify pause
+  now_playing=''
+  for item in $(playerctl -a --format "{{playerName}},{{status}}" --ignore-player firefox status); do
+    if [ "$(echo "$item" | awk -F',' '{print $2}')" = "Playing" ]; then
+      player=$(echo "$item" | awk -F',' '{print $1}')
+      playerctl -p "$player" pause
+      now_playing="$now_playing $player"
+    fi
+  done
+  if [ -n "$now_playing" ]; then
     eval "$lock_cmd"
-    playerctl -p spotify play
+    for player in $now_playing; do
+      playerctl -p "$player" play
+    done
   else
     eval "$lock_cmd -f"
   fi
